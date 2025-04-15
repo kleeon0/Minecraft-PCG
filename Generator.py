5e323abe22e216ca84368f0e63906d5af1e94a14
Creon Tam <108330453+kleeon0@users.noreply.github.com>
2025-04-15 16:46:25 +0100

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -12,17 +12,33 @@ import random
 from getcoords import *
 from Heatmap import *
 
-world_path = "C:\\Users\\Creon\\AppData\\Roaming\\.minecraft\\saves\\12"
-origin = (-450, 0, -70)
+# minecraft world path
+world_path = "C:\\Users\\Creon\\AppData\\Roaming\\.minecraft\\saves\\Video Demo"
+# top left corner of the area to be generated
+origin = (0, 0, 0)
 
+# load the world
 level = amulet.load_level(world_path)
+
+# get surface coordinates from origin of the world
 coords = get_coords(level, origin)
-grouped_regions = group_similar_y_regions_from_coords(coords, min_size=10, tol=2)
+
+# group regions to clear out
+grouped_regions = group_similar_y_regions_from_coords(coords, min_size=5, tol=2)
 clear_space(level, grouped_regions)
 
+# get surface coordinates from flattened out world
+coords = get_coords(level, origin)
+grouped_regions = group_similar_y_regions_from_coords(coords, min_size=2, tol=2)
+
+# group regions that we are going to run WFC on
+grouped_regions = [region for region in grouped_regions if region[1][0] - region[0][0]+1 >= 10 and region[1][2] - region[0][2]+1 >= 10]
+clear_space(level, grouped_regions)
+print("Generating content")
 for region in grouped_regions:
     origin = (region[0][0], region[0][2])
     base_y = region[0][1]+1
+    # get the size of the region for WFC
     width = region[1][0] - region[0][0]+1
     length = region[1][2] - region[0][2]+1
     if width >= 10 and length >= 10:
