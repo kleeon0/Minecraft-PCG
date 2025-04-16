0d57fea57ee527ed01c5e249c738bea906c45646
gmukthi <158175662+gmukthi@users.noreply.github.com>
2025-04-16 17:07:16 +0800

    Update Walls.py
    added comment + roof and backboard placements

--- build_functions/Walls.py
+++ build_functions/Walls.py
@@ -4,9 +4,11 @@ from amulet.utils.world_utils import block_coords_to_chunk_coords
 from amulet_nbt import StringTag
 
 # T = Top, Bottom, L = Left, R = Right
+# Orientation is facing North
 
+# Core function to place blocks in the world
+# Uses Amulet API to handle block placement
 def place_block(level, position, block):
-    """Place a block in the Amulet world."""
     x, y, z = position
     cx, cz = block_coords_to_chunk_coords(x, z)
     chunk = level.get_chunk(cx, cz, "minecraft:overworld")
@@ -14,79 +16,158 @@ def place_block(level, position, block):
     chunk.blocks[offset_x, y, offset_z] = level.block_palette.get_add_block(block)
     chunk.changed = True
 
+# for building the top wall
+#
 def gen_TopWall(level, position, wall, floor, height, length, width):
-    """Generate the top wall of the rectangle with floor inside."""
     x, y, z = position
-    # Create top wall
+
+    # define the block properties being used
+    slab1 = Block("minecraft", "stone_brick_slab", properties={"type": StringTag("bottom")})
+    slab2 = Block("minecraft", "stone_brick_slab", properties={"type": StringTag("top")})
+
+    # Placing the wall 
     for l in range(length):
         for h in range(height):
             place_block(level, (x + l, y + h, z), wall)
-    
-    # Place floor along the inner perimeter of the top wall
-    for l in range(0, length):  # Place along the top wall's inner perimeter (x axis)
+
+    # Placing the floor 
+    for l in range(0, length):  
         place_block(level, (x + l, y, z +1), floor)
-        for w in range(1, width):  # Place along the top wall's inner perimeter (z axis)
+        for w in range(1, width):  
             place_block(level, (x + l, y, z + w), floor)
-            place_block(level, (x + l, y+height+1, z + w), floor)
-            
+
+    x_length = x + length
+    y_height = y + height
+    z_minus = z - 1
+
+    # Placing the roof
+    for dz in range(-1, width):
+        place_block(level, (x_length - 1, y_height + 3, z + dz), slab2)
+        place_block(level, (x_length - 1, y_height + 4, z + dz), wall)
+        place_block(level, (x_length - 2, y_height + 4, z + dz), wall)
+        place_block(level, (x_length - 2, y_height + 5, z + dz), slab1)
+        place_block(level, (x_length - 3, y_height + 5, z + dz), wall)
+        place_block(level, (x_length - 4, y_height + 5, z + dz), slab1)
+        place_block(level, (x_length - 4, y_height + 4, z + dz), wall)
+        place_block(level, (x_length - 5, y_height + 4, z + dz), wall)
+        place_block(level, (x_length - 5, y_height + 3, z + dz), slab2)
+    # Topmost block underneath the roof
+    place_block(level, (x_length - 3, y_height + 4, z), floor)
+    
+    # Placing the outer frame
+    for h in range(height+1):
+        place_block(level, (x, y + h, z_minus), wall)
+        place_block(level, (x + height-1, y + h, z_minus), wall)
+
+    # Filling the backboard
+    for l in range(length):
+        for k in range(height-1):
+            place_block(level, (x + l, y_height + k, z), floor)
 
 
-
+# for building the bottom wall
+# 
 def gen_BottomWall(level, position, wall, floor, height, length, width):
-    """Generate the bottom wall of the rectangle with floor inside."""
     x, y, z = position
-    # Create bottom wall
+
+    # Define the block properties being used
+    slab1 = Block("minecraft", "stone_brick_slab", properties={"type": StringTag("bottom")})
+    slab2 = Block("minecraft", "stone_brick_slab", properties={"type": StringTag("top")})
+
+    # Placing the wall
     for l in range(length):
         for h in range(height):
             place_block(level, (x + l , y + h, z + width - 1), wall)
     
-    for l in range(0, length):  # Place along the bottom wall's inner perimeter (x axis)
-        place_block(level, (x + l, y, z + width -2), floor)
+    # Placing the floor
+    for l in range(0, length):  
+        place_block(level, (x + l, y, z + width -2), floor) 
         for w in range(width-1):
-            place_block(level, (x + l, y+height+1, z + w), floor)
-            place_block(level, (x + l, y, z + w), floor)
-            
+            place_block(level, (x + l, y, z + w), floor) 
 
-            
-        
-    
-        
+    x_length = x + length
+    y_height = y + height
+
+    # Placing the roof
+    for dz in range(0, width+1):
+        place_block(level, (x_length - 1, y_height + 3, z + dz), slab2)
+        place_block(level, (x_length - 1, y_height + 4, z + dz), wall)
+        place_block(level, (x_length - 2, y_height + 4, z + dz), wall)
+        place_block(level, (x_length - 2, y_height + 5, z + dz), slab1)
+        place_block(level, (x_length - 3, y_height + 5, z + dz), wall)
+        place_block(level, (x_length - 4, y_height + 5, z + dz), slab1)
+        place_block(level, (x_length - 4, y_height + 4, z + dz), wall)
+        place_block(level, (x_length - 5, y_height + 4, z + dz), wall)
+        place_block(level, (x_length - 5, y_height + 3, z + dz), slab2)
+    place_block(level, (x_length - 3, y_height + 4, z+l), floor)
+
+    # Backboard
+    for k in range(height-1):
+        for l in range(length):
+            place_block(level, (x + l, y_height + k, z + width - 1), floor)
+
+
+# for building the left wall
+#
 def gen_LeftWall(level, position, wall, floor, height, length, width):
-    """Generate the left wall of the rectangle with floor inside."""
     x, y, z = position
-    # Create left wall
+
+    # Define the block properties being used
+    under = Block("minecraft", "stone_brick_stairs", properties={"facing": StringTag("west"),
+                                                                "half": StringTag("top") })  
+    slab = Block("minecraft", "stone_brick_slab", properties={"type": StringTag("bottom")})
+    stair = Block("minecraft", "stone_brick_stairs", properties={"facing": StringTag("east")})
+
+    # Place the left wall
     for l in range(width):
         for h in range(height):
             place_block(level, (x, y + h, z + l), wall)
-        # Place floor along the inner perimeter of the left wall
-    for l in range(0, length):  # Place along the left wall's inner perimeter (z axis)
+
+    # Place the floor
+    for l in range(0, length):  
         place_block(level, (x+1, y, z + l), floor)
-        for w in range(1, width):  # Place along the left wall's inner perimeter (x axis)
+        for w in range(1, width):  
             place_block(level, (x + w, y, z + l), floor)
-            place_block(level, (x + w, y+height+1, z + l), floor)
-    
-        stair = Block("minecraft", "stone_brick_stairs", properties={"facing": StringTag("east")})
-        place_block(level, (x, y + height, z+l), stair)
-        place_block(level, (x-1, y + height-1, z+l), stair)
-        place_block(level, (x+1, y + height+1, z+l), stair)
         
+        # Placing the roof 
+        y_height = y + height  
+        place_block(level, (x, y_height, z+l), stair)
+        place_block(level, (x+1, y_height + 1, z+l), stair)
+        place_block(level, (x+2, y_height + 2, z+l), stair)
+        place_block(level, (x+3, y_height + 3, z+l), stair)
+        place_block(level, (x+4, y_height + 3, z+l), under)
+        place_block(level, (x+4, y_height + 4, z+l), slab)
 
 
-
+# for building the right wall
+#
 def gen_RightWall(level, position, wall, floor, height, length, width):
     x, y, z = position
-    # Create right wall
+
+    # Define the block properties being used
+    under = Block("minecraft", "stone_brick_stairs", properties={"facing": StringTag("east"),
+                                                                "half": StringTag("top") })  
+    slab = Block("minecraft", "stone_brick_slab", properties={"type": StringTag("bottom")})
+    stair = Block("minecraft", "stone_brick_stairs", properties={"facing": StringTag("west")})
+    
+    # Placing the wall
     for l in range(width):
         for h in range(height):
             place_block(level, (x + length - 1, y + h, z + l), wall)
-    for l in range(0, length):  # Place along the right wall's inner perimeter (z axis)
+    
+    # Placing the floor
+    for l in range(0, length):  
         place_block(level, (x + length - 2, y, z + l), floor)
-        for w in range(width-1):  # Place along the left wall's inner perimeter (x axis)
+        for w in range(width-1):  
             place_block(level, (x + w, y, z + l), floor)
-            place_block(level, (x + w, y+height+1, z + l), floor)
 
-        stair = Block("minecraft", "stone_brick_stairs", properties={"facing": StringTag("west")})
-        place_block(level, (x+length-1, y + height, z+l), stair)
-        place_block(level, (x+length, y + height-1, z+l), stair)
-        place_block(level, (x+length-2, y + height+1, z+l), stair)
-
+        # Placing the roof
+        x_length = x + length
+        y_height = y + height
+        place_block(level, (x_length-1, y_height, z+l), stair)
+        place_block(level, (x_length-2, y_height + 1, z+l), stair)
+        place_block(level, (x_length-3, y_height + 2, z+l), stair)
+        place_block(level, (x_length-4, y_height + 3, z+l), stair)
+        place_block(level, (x_length-5, y_height + 3, z+l), under)
+        place_block(level, (x_length-5, y_height + 4, z+l), slab)
+        
